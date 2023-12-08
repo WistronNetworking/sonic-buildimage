6d82cbf256ee439f34e0d875306fbaf876dc9e94
RogerChang <Roger_Chang@wistron.com>
2023-12-08 11:15:03 +0800

    Bugzilla#0000 - put all wistron platform in one folder
    [Bug/Feature]
    
    [Root Causes]
    
    [Solution]

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -7,9 +7,7 @@ include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/mrvl-prestera.mk
 include $(PLATFORM_PATH)/platform-marvell.mk
-include $(PLATFORM_PATH)/platform-es2227-54ts.mk
-include $(PLATFORM_PATH)/platform-es2227-54ts-p.mk
-include $(PLATFORM_PATH)/platform-es1227-54ts.mk
+include $(PLATFORM_PATH)/platform-wistron.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
