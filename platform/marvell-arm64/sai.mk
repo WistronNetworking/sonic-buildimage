a933dd1561b6bcaa066d30d678ce964fd22258fe
RogerChang <Roger_Chang@wistron.com>
2023-12-08 15:57:00 +0800

    Bugzilla#0000 - add local sai package folder
    [Bug/Feature]
    
    [Root Causes]
    
    [Solution]

--- platform/marvell-arm64/sai.mk
+++ platform/marvell-arm64/sai.mk
@@ -3,7 +3,21 @@
 export MRVL_SAI_VERSION = 1.11.0-9
 export MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
+MRVL_SAI_DEB_LOCAL_URL = $(SRC_PATH)/mrvl_sai/
+export MRVL_SAI_DEB_LOCAL_URL
+#
+ifneq ($(MRVL_SAI_DEB_LOCAL_URL), )
+SAI_FROM_LOCAL = y
+else
+SAI_FROM_LOCAL = n
+endif
+
+ifeq ($(SAI_FROM_LOCAL), y)
+$(MRVL_SAI)_PATH = $(MRVL_SAI_DEB_LOCAL_URL)/$(MRVL_SAI_VERSION)
+SONIC_COPY_DEBS += $(MRVL_SAI)
+$(eval $(call add_conflict_package,$(MRVL_SAI),$(LIBSAIVS_DEV)))
+else
 $(MRVL_SAI)_SRC_PATH = $(PLATFORM_PATH)/sai
 $(eval $(call add_conflict_package,$(MRVL_SAI),$(LIBSAIVS_DEV)))
-
 SONIC_MAKE_DEBS += $(MRVL_SAI)
+endif
