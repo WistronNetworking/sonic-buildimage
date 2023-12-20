fd230128dcbca9b6afed63a087da4f737219ec49
RogerChang <Roger_Chang@wistron.com>
2023-12-20 16:40:23 +0800

    Bugzilla#0000 - correct the ES1227/ES2227 initial script
    [Bug/Feature]
    Bug
    
    [Root Causes]
    should use wistron psu driver
    
    [Solution]
    correct it

--- platform/marvell-arm64/sonic-platform-wistron/es1227_54ts/scripts/es1227_54ts-init.sh
+++ platform/marvell-arm64/sonic-platform-wistron/es1227_54ts/scripts/es1227_54ts-init.sh
@@ -14,7 +14,7 @@ load_kernel_drivers() {
     modprobe i2c_dev
 	insmod /usr/lib/modules/5.10.0-18-2-arm64/kernel/extra/wistron_cpld.ko
     insmod /usr/lib/modules/5.10.0-18-2-arm64/kernel/extra/wistron_max31790.ko
-    insmod /usr/lib/modules/5.10.0-18-2-arm64/kernel/extra/wistron_eeprom.ko
+    insmod /usr/lib/modules/5.10.0-18-2-arm64/kernel/extra/wistron_psu.ko
     insmod /usr/lib/modules/5.10.0-18-2-arm64/kernel/extra/mvcpss.ko
 	modprobe optoe
 	modprobe jc42
