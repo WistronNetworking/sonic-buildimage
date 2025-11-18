9ab17305d4dff48fc74add51118e489597ceee0c
Owen Wu <owen_wu@wistron.com>
2025-11-18 19:52:14 +0800

    Add Wistron SONiC platform
    Signed-off-by: Owen Wu <owen_wu@wistron.com>

---
+++ platform/marvell-prestera/sonic-platform-wistron/es1227_36ts_p/scripts/es1227_36ts_p_plt_setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+fw_uboot_env_cfg()
+{
+    echo "Setting up U-Boot environment..."
+    MACH_FILE="/host/machine.conf"
+    PLATFORM=`sed -n 's/onie_platform=\(.*\)/\1/p' $MACH_FILE`
+}
+
+es1227_36ts_p_profile()
+{
+    MAC_ADDR=$(fw_printenv -n ethaddr)
+    find /usr/share/sonic/device/*es1227_36ts_p* -name profile.ini | xargs sed -i "s/switchMacAddress=.*/switchMacAddress=$MAC_ADDR/g"
+    echo "es1227_36ts_p: Updating switch mac address ${MAC_ADDR}"
+}
+
+update_modulelist()
+{
+    MODULE_FILE="/etc/modules-load.d/marvell.conf"
+
+    echo "# Module list to load during the boot" > $MODULE_FILE
+    echo "mvcpss" >> $MODULE_FILE
+    echo "psample" >> $MODULE_FILE
+}
+
+main()
+{
+    fw_uboot_env_cfg
+    es1227_36ts_p_profile
+    update_modulelist
+}
+
+main $@
